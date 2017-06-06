bec2819c25b0c06adf736db328f17a41175a9b73
Brad <bjpollard94@gmail.com>
2017-06-06 12:25:44 +0100

    Added Puppet install lines

--- puppetModuleScript.sh
+++ puppetModuleScript.sh
@@ -1,3 +1,9 @@
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
+sudo dpkg -i puppetlabs-release-pc1-trusty.deb
+sudo apt-get -y update
+sudo apt-get install -y puppetserver
+sudo service puppetserver start
+
 cd /etc/puppet/modules
 sudo mkdir apache #module name
 
